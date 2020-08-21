738f1ee0f537eb0af052727addb2485ac287c2a6
clamchowder <lamchcl@gmail.com>
2020-08-21 20:22:50 +0000

    added a half-functional navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,72 @@
 /** @jsx jsx */
-import { Box, Text, jsx } from 'theme-ui'
+import { Box, Text, Image, Grid, NavLink, jsx } from 'theme-ui'
 import useAuth from '../util/auth'
 import Link from './link'
+import logo from '../../static/climatedu.png'
+
+const links = [
+  {
+    'location':_=>'/',
+    'text':_=>'Home'
+  },
+  {
+    'location':_=>'/about',
+    'text':_=>'About',
+    'dropdown':['Our Team','Our Mission']
+  },
+  {
+    'location':_=>'/contact',
+    'text':_=>'Contact Us'
+  },
+  {
+    'location':user=>user?'/profile':'/login',
+    'text':user=>user?'Welcome '+user.displayName+'!':'Log in'
+  },
+];
 
 const Navbar = () => {
   const user = useAuth()
   return (
-    <Box
+    <Grid
+      gap={0}
+      columns={['repeat(2,1fr)','repeat(5,1fr)']}
       sx={{
         backgroundColor: 'background',
-        mb: 4,
+        justifyItems: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        fontSize: '24px',
+        maxWidth: '960px',
+        margin: 'auto',
+        padding: '5px'
       }}
     >
-      this will be replaced with the real navbar
-      {user === null ? (
-        <Link to='/login'>login and register</Link>
-      ) : (
-        <Text>welcome {user.displayName}</Text>
-      )}
-    </Box>
+      <Image
+        src={logo}
+        alt='climatedu logo'
+        sx={{
+          minHeight:'3em',
+          maxHeight:'6em',
+          justifySelf:'start'
+        }}
+      />
+      {links.map(({location,text}) => {
+        const cur = window.location.pathname.endsWith(location())
+        return (<Box sx={{
+          display: 'inline-block',
+          padding: '2px 8px',
+          borderRadius: '10px',
+          background: cur?'#dfd':'inherit',
+          border: cur?'3px solid green':'0px',
+        }}>
+          <NavLink
+            href={location()}
+          >
+            {text()}
+          </NavLink>
+        </Box>)
+      })}
+    </Grid>
   )
 }
 
