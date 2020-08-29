f4f2ffc4a813fac255924edc942789969909cce6
Darin Mao <maodarin@gmail.com>
2020-08-28 23:51:51 -0400

    feat: add temporary login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,56 @@
+/** @jsx jsx */
 import { useCallback, useState } from 'react'
 import { Redirect } from '@reach/router'
-import { jsx, Box, Text, Heading } from 'theme-ui'
-import Button from '../components/button'
+import { Button, Flex, Input, Text, Heading, jsx } from 'theme-ui'
+import { graphql } from 'gatsby'
 
 import { AiFillGoogleCircle } from 'react-icons/ai'
 
 import firebase from 'firebase/app'
 import useFirebase from '../firebase/useFirebase'
 import useAuth from '../util/auth'
-/** @jsx jsx */
 
-export default function login() {
+import Layout from '../components/layout'
+import PageHeader from '../components/pageheader'
+import Container from '../components/container'
+
+const Login = ({ data }) => {
+  if (!data.configYaml.enableLogin) {
+    return (
+      <Layout>
+        <PageHeader
+          primary='Login'
+          secondary="We're still building this course. Come back and join us in October!"
+        />
+        <Container>
+          <Heading
+            sx={{
+              textAlign: 'center',
+              mb: 3,
+            }}
+          >
+            Leave your email, and we&apos;ll remind you:
+          </Heading>
+          <Flex
+            as='form'
+            sx={{
+              maxWidth: 300,
+              m: 'auto',
+              flexDirection: 'column',
+            }}
+          >
+            <Input
+              placeholder='Email'
+              sx={{
+                mb: 3,
+              }}
+            />
+            <Button>Submit</Button>
+          </Flex>
+        </Container>
+      </Layout>
+    )
+  }
   const firebaseApp = useFirebase()
   const [error, setError] = useState(null)
 
@@ -59,3 +99,13 @@ export default function login() {
     </Box>
   )
 }
+
+export const query = graphql`
+  {
+    configYaml {
+      enableLogin
+    }
+  }
+`
+
+export default Login
