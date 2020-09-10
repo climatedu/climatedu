f1504f71c15f73ae8d8097f74e3b3739b172432f
Kevin Higgs <github@kevinhiggs.com>
2020-09-10 16:17:16 -0400

    fix: lint

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,34 +1,33 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { useLocation } from '@reach/router'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const SEO = ({ title, description }) => {
   const { pathname } = useLocation()
   const { site } = useStaticQuery(query)
-  
+
   const {
     defaultTitle,
     titleTemplate,
     defaultDescription,
     siteUrl,
-    twitterUsername,
   } = site.siteMetadata
-  
+
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
     url: `${siteUrl}${pathname}`,
   }
-  
+
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <meta name="description" content={seo.description} />
-      {seo.url && <meta property="og:url" content={seo.url} />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
+      <meta name='description' content={seo.description} />
+      {seo.url && <meta property='og:url' content={seo.url} />}
+      {seo.title && <meta property='og:title' content={seo.title} />}
       {seo.description && (
-        <meta property="og:description" content={seo.description} />
+        <meta property='og:description' content={seo.description} />
       )}
     </Helmet>
   )
