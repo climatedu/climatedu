468e9ffc745549ca705551e18bbf6ad1b397a1af
vivieli77 <vivieli77@gmail.com>
2021-03-02 22:38:59 -0500

    fixing the WHITE TEACHER RESPONSES SCREEN

--- src/pages/responses.js
+++ src/pages/responses.js
@@ -103,7 +103,7 @@ const Responses = ({ location }) => {
                                 }}
                                 name='feedback'
                                 id='feedback'
-                                defaultValue={responses.feedback[question.key]}
+                                defaultValue={responses?.feedback[question.key]}
                                 placeholder='Leave your feedback here'
                               />
                               <Button
